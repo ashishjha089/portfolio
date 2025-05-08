218afc7f92a63da46cfcb274f9ae2a685803bb4b
ashishjha089 <ajha91591@gmail.com>
2025-05-08 17:19:54 +0530

    install icon and tailwind package

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-       <h1>hello bro</h1>
+        <h1 class="text-3xl text-red-500 font-bold  underline">
+    Hello world!
+  </h1>
      </>
   )
 }
