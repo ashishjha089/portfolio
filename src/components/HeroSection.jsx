ae08f06088008b878902389b12bd103a09588935
ashishjha089 <ajha91591@gmail.com>
2025-12-27 18:01:16 +0530

    changes in footer

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-import LovableAvatar from "./LovableAvatar";
+import LovableAvatar from "./AshishAvatar";
 
 export default function HeroSection() {
   return (
@@ -12,14 +12,15 @@ export default function HeroSection() {
       </div>
 
       <div className="flex-1 space-y-6 animate-fade-in">
-        <h1 className="text-4xl md:text-5xl font-bold  mb-2 text-gray-800 tracking-wide">
-          Hey, I’m <span className="text-purple-600 ">Ashishkumar Jha</span>!
-        </h1> 
+        <h1 className="text-4xl md:text-5xl font-bold  mb-2 text-gray-800 tracking-wide ">
+          Hey, I’m{" "}
+          <span className="text-purple-600  gap-10">Ashishkumar Jha</span>!
+        </h1>
         <p className="text-lg md:text-xl text-gray-600 max-w-lg mx-auto md:mx-0">
-          Front-End Web Developer from India. I craft seamless,
-          beautiful, and creative websites with expertise in React.js, Firebase,
-          and Supabase. Passionate about user experience, I'm currently
-          available for hire to bring your web vision to life.
+          Front-End Web Developer from India. I craft seamless, beautiful, and
+          creative websites with expertise in React.js, Firebase, and Supabase.
+          Passionate about user experience, I'm currently available for hire to
+          bring your web vision to life.
         </p>
         <a
           href="#portfolio"
