b7ae2725395e74167484d5c29a5e66aa759b49f0
ashishjha089 <ajha91591@gmail.com>
2025-12-27 18:01:16 +0530

    changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
-    return (
-      <footer className="mt-10 mb-4 w-full flex flex-col items-center text-gray-400 text-sm animate-fade-in">
-        <span>
+  return (
+    <footer className="mt-10 mb-4 w-full flex flex-col items-center text-gray-400 text-sm animate-fade-in">
+      {/* <span>
           Made with <span className="text-purple-400">♥</span> and a sprinkle of AI
-        </span>
-        <span className="mt-1">© {new Date().getFullYear()} Ashishkumar Jha</span>
-      </footer>
-    );
-  }
+        </span> */}
+      <span className="mt-1">© {new Date().getFullYear()} Ashishkumar Jha</span>
+    </footer>
+  );
+}
